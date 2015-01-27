3b1e48fa38af2280dcb0c46bb23e61c36949432b
bgolde5 <golden.bradley@gmail.com>
2015-01-26 18:47:39 -0600

    Added basic information requirements

--- cs251/program-1/ngrams.cpp
+++ cs251/program-1/ngrams.cpp
@@ -1,1 +1,23 @@
-//program 1
+/*  ------------------------------------------------ 
+ * Analyzes 100 MB of the Google NGrams dataset 
+ *
+ * Class: CS 251, Spring 2015.  Thur 11am lab.
+ * System: vi on Mac OSX
+ * Author: Bradley Golden
+ *
+ * ToDo: tree traversal needs to be written
+ * -------------------------------------------------
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argv, char** argc){
+
+  printf("\n\n\t\tAuthor:\tBradley Golden\n");
+  printf("\t\tLab:\t Thur 11am\n");
+  printf("\t\tProgram: #1, My Word\n\n\n");
+  
+
+  return 0;
+}
