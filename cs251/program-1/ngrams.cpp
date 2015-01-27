9aacd6206b4e6e69a99535404fa7185604d31a8c
bgolde5 <golden.bradley@gmail.com>
2015-01-26 19:00:39 -0600

    added dataset

--- cs251/program-1/ngrams.cpp
+++ cs251/program-1/ngrams.cpp
@@ -1,11 +1,11 @@
 /*  ------------------------------------------------ 
+ *
  * Analyzes 100 MB of the Google NGrams dataset 
  *
  * Class: CS 251, Spring 2015.  Thur 11am lab.
  * System: vi on Mac OSX
  * Author: Bradley Golden
  *
- * ToDo: tree traversal needs to be written
  * -------------------------------------------------
  */
 
@@ -14,9 +14,9 @@
 
 int main(int argv, char** argc){
 
-  printf("\n\n\t\tAuthor:\tBradley Golden\n");
-  printf("\t\tLab:\t Thur 11am\n");
-  printf("\t\tProgram: #1, My Word\n\n\n");
+  printf("\n\n\nAuthor: Bradley Golden\n");
+  printf("Lab: Thur 11am\n");
+  printf("Program: #1, Google NGram word count\n\n\n");
   
 
   return 0;
