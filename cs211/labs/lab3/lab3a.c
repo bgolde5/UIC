cc812e64663de5876b5de207df91d17abd869053
Bradley Golden <golden.bradley@gmail.com>
2015-01-28 10:22:00 -0600

    lab corrected by professor

--- cs211/labs/lab3/lab3a.c
+++ cs211/labs/lab3/lab3a.c
@@ -13,6 +13,7 @@ typedef struct arrElemStruct
 
 void init1(arrElem el)
 {
+  /* create three random values for fields in the structure */
   el.value1 = (double)(rand()) * rand() / rand();
   el.value2 = rand() % 100;
   el.value3 = rand() % 1000;
@@ -22,6 +23,7 @@ void init1(arrElem el)
 
 void init2(arrElem* el)
 {
+  /* create three random values for fields in the structure */
   el->value1 = (double)(rand()) * rand() / rand();
   el->value2 = rand() % 100;
   el->value3 = rand() % 1000;
@@ -33,6 +35,7 @@ arrElem init3()
 {
   arrElem el;
 
+  /* create three random values for fields in the structure */
   el.value1 = (double)(rand()) * rand() / rand();
   el.value2 = rand() % 100;
   el.value3 = rand() % 1000;
@@ -73,41 +76,32 @@ int main (int argc, char**argv)
   arrElem* a3;
   int i;
 
-  printf ("Calling init1()\n");
+  printf ("\nCalling init2()with a1\n");
   for (i = 0 ; i < SIZE; ++i)
     {
      printf ("For position %d\n", i);
-     init1 (a1[i]);                     /* FIRST CALL */
-     printf ("In main(): value1: %14.5f, value2: %6d, value3: %6d\n",
-             a1[i].value1, a1[i].value2, a1[i].value3);
-    }
-
-  printf ("\nRepeating with init2()\n");
-  for (i = 0 ; i < SIZE; ++i)
-    {
-     printf ("For position %d\n", i);
-     init2 (&(a1[i]));                 /* SECOND CALL */
+     init2 (&(a1[i]));                 /* FOURTH CALL */
      printf ("In main(): value1: %14.5f, value2: %6d, value3: %6d\n",
              a1[i].value1, a1[i].value2, a1[i].value3);
     }
 
 
-  printf ("\nRepeating call to init2() with a2\n");
+  printf ("\nCalling init2() with a2\n");
 
   for (i = 0 ; i < SIZE; ++i)
     {
      printf ("For position %d\n", i);
-     init2 (a2[i]);                  /* THIRD CALL */
+     init2 (a2[i]);                  /* FIFTH CALL */
      printf ("In main(): value1: %14.5f, value2: %6d, value3: %6d\n",
              a2[i]->value1, a2[i]->value2, a2[i]->value3);
     }
 
-  printf ("\nRepeating call to init2() with a3\n");
+  printf ("\nCalling init2() with a3\n");
 
   for (i = 0 ; i < SIZE; ++i)
     {
      printf ("For position %d\n", i);
-     init2 (&(a3[i]));                  /* FOURTH CALL */
+     init2 (&(a3[i]));                  /* SIXTH CALL */
      printf ("In main(): value1: %14.5f, value2: %6d, value3: %6d\n",
              a3[i].value1, a3[i].value2, a3[i].value3);
     }
