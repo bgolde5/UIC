029413cc0e06252aeef8695fb06e673557b9739e
Bradley Golden <golden.bradley@gmail.com>
2016-04-04 22:42:59 -0500

    Player is now fully functional with the AudioService 'Server'. This means play, pause, stop, and resume all work.

--- cs478/project4/AudioClient/app/src/main/java/me/bradleygolden/audioclient/PlayerClientMainActivity.java
+++ cs478/project4/AudioClient/app/src/main/java/me/bradleygolden/audioclient/PlayerClientMainActivity.java
@@ -5,6 +5,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.media.Image;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
@@ -22,23 +23,104 @@ public class PlayerClientMainActivity extends Activity {
     private boolean mIsBound = false;
     private IAudioService mIAudioService;
 
+    private ImageButton playBtn;
+    private ImageButton pauseBtn;
+    private ImageButton prevBtn;
+    private ImageButton nextBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_client_main);
 
-        ImageButton playBtn = (ImageButton)findViewById(R.id.playBtn);
+        // Configure play button
+        playBtn = (ImageButton)findViewById(R.id.playBtn);
         playBtn.setOnClickListener(playListener);
+
+        // Configure pause button
+        pauseBtn = (ImageButton)findViewById(R.id.pauseBtn);
+        pauseBtn.setOnClickListener(pauseListener);
+
+        //Configure prev button
+        prevBtn = (ImageButton)findViewById(R.id.skipPrevBtn);
+        prevBtn.setOnClickListener(prevListener);
+
+        // Configure next button
+        nextBtn = (ImageButton)findViewById(R.id.skipNextBtn);
+        nextBtn.setOnClickListener(nextListener);
     }
 
+    // Play button listener
     View.OnClickListener playListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             if(mIsBound){
                 try {
                     mIAudioService.playClip("Ocean Motion");
+
+                    // Hide the play button, show the pause button
+                    playBtn.setVisibility(View.INVISIBLE);
+                    pauseBtn.setVisibility(View.VISIBLE);
+
                     Log.e(TAG, "Playing song");
-                } catch (RemoteException e){
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+//    // Pause button listener
+    View.OnClickListener pauseListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(mIsBound){
+                try {
+                    mIAudioService.pauseClip();
+
+                    // Hide the pause button, show the play button
+                    pauseBtn.setVisibility(View.INVISIBLE);
+                    playBtn.setVisibility(View.VISIBLE);
+
+                    Log.e(TAG, "Pausing song");
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    // Previous button listener
+    View.OnClickListener prevListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(mIsBound){
+                try {
+                    // If the song is playing, stop it first before going to the previous song
+                    if(mIAudioService.isPlaying()){
+                        mIAudioService.stopClip();
+                        pauseBtn.setVisibility(View.INVISIBLE);
+                        playBtn.setVisibility(View.VISIBLE);
+                    } else {
+                        // TODO Implement go to previous song
+                    }
+
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    // Next button listener
+    View.OnClickListener nextListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(mIsBound){
+                try {
+                    // If the song is playing, skip to the next one regardless
+                    // TODO Implement go to next song
+                } catch (Exception e){
                     e.printStackTrace();
                 }
             }
@@ -66,10 +148,28 @@ public class PlayerClientMainActivity extends Activity {
         }
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if(mIsBound) {
+            unbindService(mConnection);
+            mIsBound = false;
+        }
+    }
+
     ServiceConnection mConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
             mIAudioService = IAudioService.Stub.asInterface(service);
+
+
+            if (mIAudioService == null) {
+                Log.e(TAG, "Service not connected");
+
+            } else {
+                Log.e(TAG, "Service connected");
+            }
         }
 
         @Override
---
+++ cs478/project4/AudioClient/app/src/main/java/me/bradleygolden/audioclient/Song.java
@@ -0,0 +1,7 @@
+package me.bradleygolden.audioclient;
+
+/**
+ * Created by bradleygolden on 4/4/16.
+ */
+public class Song {
+}
